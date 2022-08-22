f08f360c8d40e8ef424e2414f7b8f51d206124cb
Borisenko, Dmitry <borisenkodv@gmail.com>
2022-08-22 22:10:44 +0600

    Add: minimal boilerplate code

---
+++ src/test/java/com/pydog/hdemo/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pydog.hdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
